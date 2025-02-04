4c8c888e13cb1fb1931bf3914bf95442f0eec684
RbCaVi <robert@vail-family.com>
2025-02-04 10:05:49 -0500

    extraversed

--- crismas.rs
+++ crismas.rs
@@ -1,12 +1,19 @@
 // crismas
 
 fn main() {
-    // i didn't put the rest of the lines :(
     let lines = [
         "a partridge in a pear tree",
         "two turtle doves, and ",
         "three french hens, ",
         "four calling birds, ",
+        "five golden rings, ",
+        "six geese a laying, ",
+        "seven swans a swimming, ",
+        "eight maids a milking, ",
+        "nine ladies dancing, ",
+        "ten lords a leaping, ",
+        "eleven pipers piping, ",
+        "twelve drummers drumming, ",
     ];
     
     let ordinals = [
@@ -14,9 +21,17 @@ fn main() {
         "second",
         "third",
         "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth",
     ];
     
-    for verse in 0..4 {
+    for verse in 0..12 {
         print!("On the {} day of Christmas, my true love gave to me, ", ordinals[verse]);
         for line in (0..verse + 1).rev() {
             print!("{}", lines[line]);
